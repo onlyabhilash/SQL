f1a1c4c39ce3d02694fe36384501a32d42664b71
onlyabhilash <77659887+onlyabhilash@users.noreply.github.com>
2021-09-28 19:09:35 +0530

    sql

--- PortfolioProject.sql
+++ PortfolioProject.sql
@@ -1,5 +1,6 @@
 SELECT *
 FROM PortfolioProject..CovidDeaths
+WHERE continent is not null
 ORDER BY 3,4
 
 --SELECT *
@@ -27,8 +28,32 @@ ORDER By 1,2
 
 SELECT Location, date, Population, total_cases, (total_cases/Population) * 100  As PercentagePopulationInfected
 FROM PortfolioProject..CovidDeaths
-WHERE location like  '%states%'
+-- WHERE location like  '%states%'
 ORDER By 1,2 
 
 -- Looking at Countries with Highest Infection Rate compared to Population
 
+SELECT Location,  Population, max(total_cases) as HighestInfectionCount, max(total_cases/Population) * 100  
+	as PercentagePopulationInfected
+FROM PortfolioProject..CovidDeaths
+-- WHERE location like  '%states%'
+Group By Location, Population
+ORDER By PercentagePopulationInfected desc
+
+---Showing countries with Highest Death count per population
+
+SELECT Location,  MAX(cast(total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+-- WHERE location like  '%states%'
+WHERE continent is not null  /* It will exclude all continents*/
+Group By Location, Population
+ORDER By TotalDeathCount desc
+
+-- Lets break this down by continent
+
+SELECT Location,  MAX(cast(total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+-- WHERE location like  '%states%'
+WHERE continent is not null  /* It will exclude all continents*/
+Group By Location, Population
+ORDER By TotalDeathCount desc
